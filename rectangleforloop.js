344970f8f589226ed5a6eafad75a50be86c64a2c
PACIFIQUE-VIBUCODER <imanirumvapacifique39@gmail.com>
2026-06-21 20:43:24 +0200

    for loop  examples

---
+++ rectangleforloop.js
@@ -0,0 +1,9 @@
+const width = 5;
+const height = 5;
+for(let a = 0; a<=height; a++){
+    let result = "";
+    for(let b = 0; b<width; b++){
+        result += "*";
+    }
+    console.log(result);
+}
